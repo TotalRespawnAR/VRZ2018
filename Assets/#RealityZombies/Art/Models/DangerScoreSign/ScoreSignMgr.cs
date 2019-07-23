3d5ea5189a7753ddd644a14a7b375b5f4f805569
nablam <lamribenn@gmail.com>
2019-07-23 12:11:57 -0400

    4

---
+++ Assets/#RealityZombies/Art/Models/DangerScoreSign/ScoreSignMgr.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class ScoreSignMgr : ShotObject
+{
+    public Animator anim;
+    public TextMesh DisplayScore;
+    public TextMesh DisplayWaveNumber;
+    protected bool lowered = true;
+    private GameManager manager;
+
+    void Awake()
+    {
+        Lift();
+        lowered = false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        manager = GameManager.Instance;
+        //
+        Lower();
+    }
+
+    void Update()
+    {
+        //if (!lowered && Time.time > .5f) {
+        //	Lower();
+        //	lowered = true;
+        //}
+        if (!manager.IsPlayerDead)
+        {
+            SetScoreDisplay(manager.GetScoreMAnager().Get_PointsTotal().ToString());
+        }
+    }
+
+    public void Lower()
+    {
+        if (!lowered)
+        {
+            // Debug.Log("Lowering Sign");
+            // PlayHeadShotSound.Instance.PlayScoreSound("_ScoreSignMove");
+            anim.SetBool("Lowered", true);
+            lowered = true;
+        }
+    }
+
+    public void Lift()
+    {
+        if (lowered)
+        {
+            //   Debug.Log("lifiting Sign");
+            // PlayHeadShotSound.Instance.PlaySplatSound("_ScoreSignMove");
+            anim.SetBool("Lowered", false);
+            lowered = false;
+        }
+    }
+
+    public override void Shot()
+    {
+        base.Shot();
+        anim.SetTrigger("Shake");
+    }
+
+    public void SetScoreDisplay(string argScore)
+    {
+        DisplayScore.text = argScore;
+    }
+
+    public void SetWaveNumberDisplay(string argWaveNum)
+    {
+        DisplayWaveNumber.text = argWaveNum;
+    }
+
+
+}
---
+++ Assets/#RealityZombies/Art/Models/DangerScoreSign/ShotObject.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class ShotObject : MonoBehaviour, IShootable
+{//MonoBehaviour {
+
+    bool wasShot = false;
+    public Transform _transform;
+    public new Transform transform
+    {
+        get
+        {
+            if (_transform == null)
+            {
+                _transform = GetComponent<Transform>();
+            }
+            return _transform;
+        }
+    }
+
+    public Renderer myRenderer;
+
+    public GameObject replacementPrefab;
+    public GameObject particlesPrefab;
+
+    protected GameObject replacement;
+    protected GameObject particles;
+
+    public bool givesScore = true;
+
+    //TODO: hookup sound
+    public AudioClip sound;
+
+    // Use this for initialization
+    //void Start () {
+    public virtual void Shot()
+    {
+
+    }
+    public void Reset()
+    {
+        if (myRenderer != null)
+            myRenderer.enabled = true;
+        if (replacement != null)
+            replacement.SetActive(false);
+
+    }
+
+    public void Shot(Bullet argBullet)
+    {
+        if (wasShot) return;
+        if (replacement == null)
+        {
+            if (replacementPrefab != null)
+            {
+                replacement = Instantiate(replacementPrefab, transform.position, transform.rotation, transform);
+                replacement.transform.localScale = transform.localScale;
+            }
+        }
+        else
+            replacement.SetActive(true);
+        if (myRenderer == null)
+            myRenderer = GetComponent<Renderer>();
+        if (myRenderer != null)
+            myRenderer.enabled = false;
+        if (particles == null && particlesPrefab != null)
+        {
+            //particles = Instantiate(particlesPrefab, transform.position, transform.rotation, transform.parent);
+            particles = Instantiate(particlesPrefab, transform.position, transform.rotation, transform);
+            //particles = Instantiate(particlesPrefab, Vector3.zero, Quaternion.identity, transform);
+            //particles = Instantiate(particlesPrefab, transform.localPosition, transform.localRotation, transform);
+            Destroy(particles, 5f);
+        }
+        wasShot = true;
+        PlayHeadShotSound.Instance.PlayScoreSound("_BulbBreak");
+        GameManager.Instance.AirHornEffect();
+    }
+
+    public void aimed(bool argTF)
+    {
+        throw new System.NotImplementedException();
+    }
+}
