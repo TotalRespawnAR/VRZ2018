62ccdad7b52ec954d19f85310dfd07c2684f4ed7
nablam <lamribenn@gmail.com>
2019-07-24 12:12:12 -0400

    added ArenaLiveDaemon_0_14_0 folder and hapticdata.hptm and hapticdata.txt to rootfolder,  thearenaliveUnityPackage_0_14_0 only included dlls no blastermangers  i got those form the last UnityApiPack_0_13_0

---
+++ Assets/ArenaLive/Scripts/B_AutomaticWeapon.cs
@@ -0,0 +1,167 @@
+﻿using UnityEngine;
+using System.Collections;
+using ArenaLiveAPI;
+
+public class B_AutomaticWeapon : BlasterBehaviour {
+
+    public float FireRate;
+
+    public int MaxAmmo;
+    public int Ammo;
+
+    private bool IsWaitingInitial = false;
+    private bool IsFiring = false;
+
+    private float LastShotTime = 0f;
+    public float InitialDelay;
+    private float StartFiringTime = 0f;
+
+    private bool WillChangeMode = false;
+
+    public Blaster blaster;
+    public BlasterBehaviour NextBehaviour;
+
+    // Use this for initialization
+    void Start()
+    {
+        Ammo = MaxAmmo;
+    }
+
+    protected override void OnActivated()
+    {
+        ChangeMode(InitialModeId);
+        WillChangeMode = false;
+
+        if (Ammo > 0)
+        {
+            SetUserConditions(~BlasterUserConditions.OutOfAmmo);
+        }
+        else
+        {
+            SetUserConditions(BlasterUserConditions.OutOfAmmo);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!IsConnected || WillChangeMode)
+        {
+            return;
+        }
+
+        if (BlasterInput.GetButtonDown(BlasterButtons.Trigger))
+        {
+            Fire();
+        }
+
+        if (BlasterInput.GetButtonUp(BlasterButtons.Trigger))
+        {
+            IsFiring = false;
+        }
+
+        if (BlasterInput.GetButtonUp(BlasterButtons.Bottom) && !IsFiring && !IsWaitingInitial)
+        {
+            PlayModeSlot(1);
+            UpdateAmmo(MaxAmmo);
+        }
+
+        if (BlasterInput.GetButtonUp(BlasterButtons.LeftBack)
+            || BlasterInput.GetButtonUp(BlasterButtons.RightBack))
+        {
+            PrepareChangeMode();
+        }
+
+        if (IsWaitingInitial)
+        {
+            if ((Time.time - StartFiringTime) > InitialDelay)
+            {
+                IsWaitingInitial = false;
+                if (Ammo > 0)
+                {
+                    Ammo--;
+                    LastShotTime = Time.time;
+                    if (Ammo == 0)
+                    {
+                        SetUserConditions(BlasterUserConditions.OutOfAmmo);
+                    }
+                    
+                
+                    else
+                    {
+                        IsFiring = true;
+                    }
+                }
+            }
+        }
+
+        if (IsFiring)
+        {
+            if ((Time.time - LastShotTime) > FireRate)
+            {
+                if (Ammo > 0)
+                {
+                    LastShotTime = Time.time;
+                    Ammo--;
+                    if (Ammo == 0)
+                    {
+                        SetUserConditions(BlasterUserConditions.OutOfAmmo);
+                        IsFiring = false;
+                    }
+                }
+            }
+
+        }
+    }
+
+    void Fire()
+    {
+        //if(Ammo > 0)
+        //{
+            FireAuto();
+       // }
+    }
+
+    void FireAuto()
+    {
+        PlayModeSlot(0);
+            StartFiringTime = Time.time;
+        IsWaitingInitial = true;
+
+    }
+
+    void AmmoDecrementer()
+    {
+        if(BlasterInput.GetButton(BlasterButtons.Trigger))
+            UpdateAmmo(Ammo - 1);
+    }
+
+    void UpdateAmmo(int newAmmoCnt)
+    {
+        Ammo = newAmmoCnt;
+
+        if(Ammo <= 0)
+        {
+            SetUserConditions(BlasterUserConditions.OutOfAmmo);
+        }
+
+        else
+        {
+            SetUserConditions(~BlasterUserConditions.OutOfAmmo);
+        }
+    }
+
+    void PrepareChangeMode()
+    {
+        WillChangeMode = true;
+
+        StartCoroutine(ChangeModeRoutine());
+    }
+
+    IEnumerator ChangeModeRoutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        blaster.Behaviour = NextBehaviour;
+    }
+}
---
+++ Assets/ArenaLive/Scripts/BlasterOperator.cs
@@ -0,0 +1,113 @@
+﻿using ArenaLiveAPI.DaemonControlling;
+using System.Collections.Generic;
+using UnityEngine;
+
+//
+// Automated Daemon Controller Example
+//
+public class BlasterOperator : MonoBehaviour
+{
+
+    string PathToDaemon;
+
+    public string[] BlasterHubComPortNames;
+
+    private int GameID;
+
+    private Dictionary<BlasterAddress, int> Pairs;
+
+    DaemonController Controller;
+
+    void Awake()
+
+
+    {
+        PathToDaemon = Application.dataPath + "/../ArenaLiveDaemon_0_14_0/Release/ArenaLiveDaemon.exe";
+        Controller = new DaemonController("GDC", PathToDaemon, true);
+
+        Controller.BlasterConnected += OnBlasterConnected;
+        Controller.BlasterDisconnected += OnBlasterDisconnected;
+        Controller.ErrorOccured += OnErrorOccured;
+        Controller.GameConnected += OnGameConnected;
+        Controller.GameDisconnected += OnGameDisconnected;
+        Controller.ConnectToDaemon();
+
+        Pairs = new Dictionary<BlasterAddress, int>();
+
+        // Connect to one or more base stations
+        foreach (string s in BlasterHubComPortNames)
+        {
+            Controller.ConnectToBlasterHub(s);
+        }
+    }
+
+    void OnGameDisconnected(int value)
+    {
+        Debug.LogFormat("GameDisconnected gameID:{0}", value);
+    }
+
+    void OnGameConnected(GameState value)
+    {
+        Debug.LogFormat("GameConnected gameID:{0} name:{1} nrPlayers:{2}", value.GameID, value.Name, value.NumberOfPlayers);
+
+        GameID = value.GameID;
+    }
+
+    void OnErrorOccured(System.Exception value)
+    {
+        Debug.LogFormat("ErrorOccured exception:{0}", value.ToString());
+    }
+
+    void OnBlasterDisconnected(BlasterAddress value)
+    {
+        Debug.LogFormat("BlasterDisconnected  addr:{0}", value.ToString());
+
+        // Here you might remove the blaster from the pairs Dictionary, 
+        // or you may leave it so that when the blaster comes back online
+        // the same connection is made
+    }
+
+    int NextPlayerID = 0;
+    void OnBlasterConnected(BlasterState value)
+    {
+        Debug.LogFormat("BlasterConnected  addr:{0} name:{1} port:{2}", value.Address.ToString(), value.Name, value.BlasterHub);
+
+        if (Pairs.ContainsKey(value.Address))
+        {
+            Controller.BindAddressToPlayerID(value.Address, GameID, Pairs[value.Address]);
+        }
+
+        else
+        {
+            Pairs.Add(value.Address, NextPlayerID);
+            Controller.BindAddressToPlayerID(value.Address, GameID, NextPlayerID++);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (Controller != null)
+        {
+            Debug.Log("Shutting down connection with Daemon");
+
+            foreach (string s in BlasterHubComPortNames)
+            {
+                Controller.DisconnectFromBlasterHub(s);
+            }
+
+            Controller.DisconnectFromDaemon();
+            Controller.Dispose();
+        }
+    }
+
+    void Update()
+    {
+        if (Controller != null)
+            Controller.Update();
+    }
+
+    public void Bind(int playerID, BlasterAddress address)
+    {
+        Controller.BindAddressToPlayerID(address, GameID, playerID);
+    }
+}
