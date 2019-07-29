7920a4b57b4d99fe9bb7c1e9a6205e527227310d
nablam <lamribenn@gmail.com>
2019-07-29 17:17:54 -0400

    addedscenes

---
+++ Assets/#VRZ/Scripts/Autostarter.cs
@@ -0,0 +1,37 @@
+﻿//#define ENABLE_LOGS
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Autostarter : MonoBehaviour
+{
+ 
+
+    public string ScenName;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(StartThisShitIn3sec());
+    }
+    IEnumerator StartThisShitIn3sec()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(ScenName);
+
+    }
+
+    //bool FindBinaryFile()
+    //{
+
+    //    Debug.Log("path to wac= " + StaticHexLogger.DirTarget);
+    //    if (File.Exists(StaticHexLogger.DirTarget + StaticHexLogger.FileName_BatchSaveBytesWACt))
+    //        return true;
+    //    return false;
+    //}
+
+
+
+}
+ 
+
---
+++ Assets/#VRZ/Scripts/ReturnToGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToGame : MonoBehaviour
+{
+    public string SceneName;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(StartInaFew());
+    }
+
+
+    IEnumerator StartInaFew()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneName);
+    }
+}
