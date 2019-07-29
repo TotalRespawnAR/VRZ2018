2467cfc952a8ac0a111fa45d21c5faeb90ea44b3
nablam <lamribenn@gmail.com>
2019-07-29 17:26:39 -0400

    start and end scenes

---
+++ Assets/#VRZ/Scripts/SceneTimerChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTimerChanger : MonoBehaviour
+{
+    public string ScenName;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(StartThisShitIn3sec());
+    }
+    IEnumerator StartThisShitIn3sec()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(ScenName);
+
+    }
+}
