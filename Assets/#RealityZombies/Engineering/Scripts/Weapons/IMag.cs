a78f9d07bf73d11c4e9ecfd4ee1d697023b8ff15
TotalRespawnDevs <mark@totalrespawnar.com>
2019-08-05 19:06:08 -0400

    mag fix

--- Assets/#RealityZombies/Engineering/Scripts/Weapons/IMag.cs
+++ Assets/#RealityZombies/Engineering/Scripts/Weapons/IMag.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public interface IMag  {
+public interface IMag
+{
 
-      bool TryDecrementBulletCount();
+    bool TryDecrementBulletCount();
     GameObject GetBulletFromMag();
     int GetBulletsCount_inMag();
     void Refill();
